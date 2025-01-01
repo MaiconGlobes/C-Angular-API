a58bec8e42b4b41cbe4543a47747728d8333843e
MaiconGlobes <maico-globes@hotmail.com>
2025-01-01 14:16:21 -0300

    Add configurations to the front and backend routes

--- Program.cs
+++ Program.cs
@@ -6,72 +6,76 @@ using BaseCodeAPI.Src.Repositories;
 using BaseCodeAPI.Src.Services;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 
-namespace BaseCodeAPI
+public class Program
 {
-   public class Program
+   public static void Main(string[] args)
    {
-      public static void Main(string[] args)
+      var builder = WebApplication.CreateBuilder(args);
+
+      ConfigServices(builder.Services);
+
+      var app = builder.Build();
+
+      app.UseSwagger();
+
+      app.UseSwaggerUI(options =>
       {
-         var builder = WebApplication.CreateBuilder(args);
+         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Net Angular");
+         options.DefaultModelsExpandDepth(-1);
+         options.InjectStylesheet("/public/swagger-ui/custom.css");
+         options.RoutePrefix = "swagger";
+      });
 
-         ConfigServices(builder.Services);
+      app.UseStaticFiles();
+      app.UseHttpsRedirection();
 
-         var app = builder.Build();
-
-         app.UseSwagger();
-
-         app.UseSwaggerUI(options =>
-         {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Net Angulary");
-            options.DefaultModelsExpandDepth(-1); 
-            options.InjectStylesheet("/public/swagger-ui/custom.css");
-            options.RoutePrefix = "swagger";
-         });
-
-         app.UseStaticFiles();
-         app.UseHttpsRedirection();
-         app.UseSpa(spa =>
+      app.UseRouting();
+      
+      app.MapWhen(context => !context.Request.Path.StartsWithSegments("/api"), spaApp =>
+      {
+         spaApp.UseSpa(spa =>
          {
             spa.Options.SourcePath = "wwwroot";
-
             if (app.Environment.IsDevelopment())
             {
                spa.UseAngularCliServer(npmScript: "start");
             }
          });
+      });
 
-         app.UseRouting();
-         app.UseCors(cors => cors.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-         app.TokenMiddlewareBuilder();
-         app.UseAuthentication();
-         app.UseAuthorization();
-         app.MapControllers();
-         app.Run("http://*:5005");
-      }
+      app.MapControllers();
+      app.UseCors(cors => cors.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+      app.TokenMiddlewareBuilder();
+      app.UseAuthentication();
+      app.UseAuthorization();
+      app.MapControllers();
+      app.Run("http://*:5005");
+   }
 
-      public static void ConfigServices(IServiceCollection services)
+   public static void ConfigServices(IServiceCollection services)
+   {
+      services.AddCors();
+      services.AddControllers();
+      services.AddSpaStaticFiles(configuration =>
       {
-         services.AddCors();
-         services.AddControllers();
-         services.AddSpaStaticFiles(configuration => { configuration.RootPath = "wwwroot/dist";});     
-         services.AddHttpContextAccessor();
-         services.AddSwaggerGen();
-         services.AddAutoMapper(typeof(AutoMapperProfile));
-         services.AddScoped<IAuthenticate, AuthenticateService>();
+         configuration.RootPath = "wwwroot/dist";
+      });
+      services.AddHttpContextAccessor();
+      services.AddSwaggerGen();
+      services.AddAutoMapper(typeof(AutoMapperProfile));
+      services.AddScoped<IAuthenticate, AuthenticateService>();
 
-         services.AddScoped<UserService>();
-         services.AddScoped<ClientService>();
-         services.AddScoped<AddressService>();
+      services.AddScoped<UserService>();
+      services.AddScoped<ClientService>();
+      services.AddScoped<AddressService>();
 
-         services.AddScoped<IRepository<UserModel>, UserRepository>();
-         services.AddScoped<IRepository<AddressModel>, AddressRepository>();
-         services.AddScoped<IRepository<ClientModel>, ClientRepository>();
+      services.AddScoped<IRepository<UserModel>, UserRepository>();
+      services.AddScoped<IRepository<AddressModel>, AddressRepository>();
+      services.AddScoped<IRepository<ClientModel>, ClientRepository>();
 
-         services.AddAuthentication();
-         services.AddAuthorization();
+      services.AddAuthentication();
+      services.AddAuthorization();
 
-         services.AddScoped<IServicesFactory, ServicesFactory>();
-      }
-
+      services.AddScoped<IServicesFactory, ServicesFactory>();
    }
 }
