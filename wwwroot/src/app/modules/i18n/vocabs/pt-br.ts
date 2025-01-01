982c8c4561dc534b7c32d23a99bf52425c09c216
MaiconGlobes <maico-globes@hotmail.com>
2025-01-01 17:05:13 -0300

    Adjustments

---
+++ wwwroot/src/app/modules/i18n/vocabs/pt-br.ts
@@ -0,0 +1,105 @@
+// Brasil
+export const locale = {
+  lang: 'pt-br',
+  data: {
+    TRANSLATOR: {
+      SELECT: 'Escolha seu idioma',
+    },
+    MENU: {
+      NEW: 'novo',
+      ACTIONS: 'Ações',
+      CREATE_POST: 'Criar nova publicação',
+      PAGES: 'Páginas',
+      FEATURES: 'Características',
+      APPS: 'Aplicativos',
+      DASHBOARD: 'Painel de Controle',
+    },
+    AUTH: {
+      GENERAL: {
+        OR: 'Ou',
+        SUBMIT_BUTTON: 'Enviar',
+        NO_ACCOUNT: 'Não tem uma conta?',
+        SIGNUP_BUTTON: 'Cadastre-se',
+        FORGOT_BUTTON: 'Esqueceu sua senha',
+        BACK_BUTTON: 'Voltar',
+        PRIVACY: 'Privacidade',
+        LEGAL: 'Legal',
+        CONTACT: 'Contato',
+      },
+      LOGIN: {
+        TITLE: 'Criar uma conta',
+        BUTTON: 'Entrar',
+      },
+      FORGOT: {
+        TITLE: 'Esqueceu sua senha?',
+        DESC: 'Digite seu e-mail para redefinir sua senha',
+        SUCCESS: 'Sua senha foi redefinida com sucesso.',
+      },
+      REGISTER: {
+        TITLE: 'Cadastro',
+        DESC: 'Preencha seus dados para criar sua conta',
+        SUCCESS: 'Sua conta foi registrada com sucesso.',
+      },
+      INPUT: {
+        EMAIL: 'E-mail',
+        FULLNAME: 'Nome completo',
+        PASSWORD: 'Senha',
+        CONFIRM_PASSWORD: 'Confirmar senha',
+        USERNAME: 'Usuário',
+      },
+      VALIDATION: {
+        INVALID: '{{name}} não é válido',
+        REQUIRED: '{{name}} é obrigatório',
+        MIN_LENGTH: 'O tamanho mínimo de {{name}} é {{min}}',
+        AGREEMENT_REQUIRED: 'Aceitar os termos e condições é obrigatório',
+        NOT_FOUND: 'O {{name}} solicitado não foi encontrado',
+        INVALID_LOGIN: 'Os dados de login estão incorretos',
+        REQUIRED_FIELD: 'Campo obrigatório',
+        MIN_LENGTH_FIELD: 'Tamanho mínimo do campo:',
+        MAX_LENGTH_FIELD: 'Tamanho máximo do campo:',
+        INVALID_FIELD: 'O campo não é válido',
+      },
+    },
+    ECOMMERCE: {
+      COMMON: {
+        SELECTED_RECORDS_COUNT: 'Número de registros selecionados: ',
+        ALL: 'Todos',
+        SUSPENDED: 'Suspenso',
+        ACTIVE: 'Ativo',
+        FILTER: 'Filtrar',
+        BY_STATUS: 'por Status',
+        BY_TYPE: 'por Tipo',
+        BUSINESS: 'Negócio',
+        INDIVIDUAL: 'Individual',
+        SEARCH: 'Buscar',
+        IN_ALL_FIELDS: 'em todos os campos',
+      },
+      ECOMMERCE: 'Comércio Eletrônico',
+      CUSTOMERS: {
+        CUSTOMERS: 'Clientes',
+        CUSTOMERS_LIST: 'Lista de clientes',
+        NEW_CUSTOMER: 'Novo Cliente',
+        DELETE_CUSTOMER_SIMPLE: {
+          TITLE: 'Excluir Cliente',
+          DESCRIPTION: 'Tem certeza de que deseja excluir este cliente permanentemente?',
+          WAIT_DESCRIPTION: 'Cliente sendo excluído...',
+          MESSAGE: 'Cliente foi excluído',
+        },
+        DELETE_CUSTOMER_MULTY: {
+          TITLE: 'Excluir Clientes',
+          DESCRIPTION: 'Tem certeza de que deseja excluir os clientes selecionados permanentemente?',
+          WAIT_DESCRIPTION: 'Clientes sendo excluídos...',
+          MESSAGE: 'Os clientes selecionados foram excluídos',
+        },
+        UPDATE_STATUS: {
+          TITLE: 'O status foi atualizado para os clientes selecionados',
+          MESSAGE: 'O status dos clientes selecionados foi atualizado com sucesso',
+        },
+        EDIT: {
+          UPDATE_MESSAGE: 'Cliente foi atualizado',
+          ADD_MESSAGE: 'Cliente foi criado',
+        },
+      },
+    },
+  },
+};
